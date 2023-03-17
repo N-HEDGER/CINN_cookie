12cd807d75ee17f074e1090ac16d4dd3cf9f48e9
N-HEDGER <nhedger1@gmail.com>
2023-03-17 14:44:34 +0000

    setup and config

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# true-neutral-cookiecutter
-A minimalist cookiecutter for research and data science projects
+# CINN_cookie
 
-Run via `cookiecutter gh:patrickmineault/true-neutral-cookiecutter`
+<img src = 'https://i.imgur.com/lg1uNwa.png' width = "200" height = "" >
+
+
+A minimalist cookiecutter for research and data science projects in the CINN.
+
+Run via `cookiecutter gh:N-HEDGER/CINN_cookie`
--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/utils.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/utils.py
@@ -3,17 +3,26 @@ import os
 import pkg_resources
 import yaml
 
+# Define the base path for the package
 base_path = os.path.dirname(os.path.dirname(
     pkg_resources.resource_filename("{{ cookiecutter.package_name }}", 'config')))
 
+# Define the path to the config file
 pkg_yaml = os.path.join(base_path, 'config', 'config.yml')
 
+# Define the paths to the subdirectories
+p_paths = dict()
 
-def load_pkg_yaml(yaml=pkg_yaml, **kwargs):
+# Loop through the subdirectories and define the paths, then add them to the dict
+for el in ['data', 'docs', 'results', 'scripts', 'tests']:
+    p_paths['{el}_dir'.format(el=el)] = os.path.join(base_path, el)
+
+
+def load_pkg_yaml(pkg_yaml=pkg_yaml, **kwargs):
     """_summary_
 
     Args:
-        yaml (_type_, optional): _description_. Defaults to pkg_yaml.
+        pkg_yaml (_type_, optional): _description_. Defaults to pkg_yaml.
 
     Returns:
         _type_: _description_
@@ -21,6 +30,7 @@ def load_pkg_yaml(yaml=pkg_yaml, **kwargs):
 
     with open(pkg_yaml, 'r') as f:
         y = yaml.safe_load(f)
+        y['p_paths'] = p_paths
 
     if 'subdict' in kwargs.keys():
         return y[kwargs['subdict']]
